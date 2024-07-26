166617d412521b9cb5c75ef6b6d14f8aa2c5f5f8
alexliqu09 <alexander.lique.l@uni.pe>
2024-07-26 01:16:45 -0300

    Add new comment to carrusel

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ function toggleLanguage() {
     const introParagraphEn = document.getElementById('intro-paragraph-en');
     const introParagraphEs = document.getElementById('intro-paragraph-es');
     const languageToggle = document.querySelector('.language-toggle');
-    
+    const enTexts = document.querySelectorAll('.en');
+    const esTexts = document.querySelectorAll('.es');
+
     if (isEnglish) {
         footerText.textContent = footerText.getAttribute('data-lang-es');
         introParagraphEn.style.display = 'none';
         introParagraphEs.style.display = 'block';
         languageToggle.textContent = 'English';
+        enTexts.forEach(el => el.style.display = 'none');
+        esTexts.forEach(el => el.style.display = 'block');
     } else {
         footerText.textContent = footerText.getAttribute('data-lang-en');
         introParagraphEn.style.display = 'block';
         introParagraphEs.style.display = 'none';
         languageToggle.textContent = 'Español';
+        enTexts.forEach(el => el.style.display = 'block');
+        esTexts.forEach(el => el.style.display = 'none');
     }
 
     isEnglish = !isEnglish;
